6ffc60e57223209c7dae78f66ecbe2d57e9451af
Rita Casiello <rituzka@gmail.com>
2020-06-13 21:25:58 +0200

    added animation resources rotate, slice down, slice up, zoom

--- Week3/BusinessCard/app/src/main/java/com/e/businesscard/MainActivity.kt
+++ Week3/BusinessCard/app/src/main/java/com/e/businesscard/MainActivity.kt
@@ -2,10 +2,16 @@ package com.e.businesscard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnJoinUs.setOnClickListener {
+
+        }
+
     }
 }
