d7d689ee90fcba6a0ed6f098239bd30047879599
Rita Casiello <rituzka@gmail.com>
2020-06-15 11:48:27 +0200

    added item share in menu

--- Week3/BusinessCard/app/src/main/java/com/e/businesscard/MainActivity.kt
+++ Week3/BusinessCard/app/src/main/java/com/e/businesscard/MainActivity.kt
@@ -13,34 +13,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //click listener to the JoinUs button
         btnJoinUs.setOnClickListener {
 
+            //animation to Android logo, it rotates 360º
             val animation = AnimationUtils.loadAnimation(this, R.anim.rotate)
             logoAndroid.startAnimation(animation)
-
+            //animation zoom to slogan "Always in Motion"
             val animation2 = AnimationUtils.loadAnimation(this, R.anim.zoom_in)
             lblAlways.startAnimation(animation2)
-
+            //animation zoom_out to label Name of the card, to give it some movement
             val animation3 = AnimationUtils.loadAnimation(this, R.anim.zoom_out)
             lblName.startAnimation(animation3)
 
         }
     }
+    //inflate view of menu in the action bar
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         super.onCreateOptionsMenu(menu)
         menuInflater.inflate(R.menu.menu, menu)
         return true
     }
-
+     //item telephone added to the menu, it shows a dialog phone info
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         super.onOptionsItemSelected(item)
         if(item.itemId == R.id.actionCall ) {
-            showInfo()
+            showInfoCall()
+        }else if(item.itemId == R.id.actionShare) {
+            showInfoShare()
         }
         return true
     }
 
-    private fun showInfo() {
+    private fun showInfoShare() {
+        val dialogshare = getString(R.string.share_image)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogshare)
+        builder.create().show()
+    }
+
+    //function to show the information of phone number and custom service attention schedule
+    private fun showInfoCall() {
         val dialogPhone = getString(R.string.call)
         val dialogSchedule = getString(R.string.schedule)
 
