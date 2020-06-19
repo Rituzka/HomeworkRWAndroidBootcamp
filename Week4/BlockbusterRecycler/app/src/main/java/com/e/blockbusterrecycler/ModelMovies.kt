bb470787989c49dab46a5721c6f3bd1bd435a69a
Rita Casiello <rituzka@gmail.com>
2020-06-19 23:09:45 +0200

    model movie class

---
+++ Week4/BlockbusterRecycler/app/src/main/java/com/e/blockbusterrecycler/ModelMovies.kt
@@ -0,0 +1,11 @@
+package com.e.blockbusterrecycler
+
+data class ModelMovies (
+    val id: Int,
+    val releaseDate: String,
+    val title: String,
+    val summary: String,
+    val poster:String,
+    val detail:String
+)
+
