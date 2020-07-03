b8ec30c163864ae88ba2d79b291f4e7533db49be
Rita Casiello <rituzka@gmail.com>
2020-07-03 20:02:00 +0200

    project homework week 6 initialized

---
+++ Week6/BlockbusterRecycler/app/src/main/java/com/e/blockbusterrecycler/ui/MovieListViewHolder.kt
@@ -0,0 +1,20 @@
+package com.e.blockbusterrecycler.ui
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.e.blockbusterrecycler.R
+import com.e.blockbusterrecycler.model.ModelMovies
+
+
+class MovieListViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    private val movieImage = itemView.findViewById<ImageView>(R.id.imageMovie)
+    private val movieTitle = itemView.findViewById<TextView>(R.id.movieTitle)
+
+    fun bindView(image: ModelMovies){
+        movieImage.setImageResource(image.poster)
+        movieTitle.text = image.title
+    }
+}
