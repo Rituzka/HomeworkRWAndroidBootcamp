7f3bae922f87b508f4c2b9e8118404726304b7b8
Rita Casiello <rituzka@gmail.com>
2020-07-11 00:58:20 +0200

    project week 7

--- Week7/BlockbusterRecycler/app/src/main/java/com/e/blockbusterrecycler/model/Movie.kt
+++ Week7/BlockbusterRecycler/app/src/main/java/com/e/blockbusterrecycler/model/Movie.kt
@@ -6,7 +6,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "movies")
-data class ModelMovies (
+data class Movie (
     @PrimaryKey val id: Int,
     val releaseDate: String,
     val title: String,
@@ -42,13 +42,13 @@ data class ModelMovies (
         return 0
     }
 
-    companion object CREATOR : Parcelable.Creator<ModelMovies> {
+    companion object CREATOR : Parcelable.Creator<Movie> {
 
-        override fun createFromParcel(parcel: Parcel): ModelMovies {
-            return ModelMovies(parcel)
+        override fun createFromParcel(parcel: Parcel): Movie {
+            return Movie(parcel)
         }
 
-        override fun newArray(size: Int): Array<ModelMovies?> = arrayOfNulls(size)
+        override fun newArray(size: Int): Array<Movie?> = arrayOfNulls(size)
     }
 }
 
