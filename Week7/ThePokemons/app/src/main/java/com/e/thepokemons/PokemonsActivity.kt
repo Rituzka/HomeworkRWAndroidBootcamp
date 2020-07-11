d61a89dd9b5490455a0a5aa739dadd8d05418e10
Rita Casiello <rituzka@gmail.com>
2020-07-11 14:58:46 +0200

    start project

---
+++ Week7/ThePokemons/app/src/main/java/com/e/thepokemons/PokemonsActivity.kt
@@ -0,0 +1,11 @@
+package com.e.thepokemons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PokemonsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemons)
+    }
+}
