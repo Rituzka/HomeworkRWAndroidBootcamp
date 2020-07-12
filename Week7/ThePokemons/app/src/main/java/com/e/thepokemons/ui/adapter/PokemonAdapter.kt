a5cdb77a25b214fe860c114049a0244e7e9813fa
Rita Casiello <rituzka@gmail.com>
2020-07-12 16:39:42 +0200

    adapter modified, set functions success and failure received data

--- Week7/ThePokemons/app/src/main/java/com/e/thepokemons/ui/adapter/PokemonAdapter.kt
+++ Week7/ThePokemons/app/src/main/java/com/e/thepokemons/ui/adapter/PokemonAdapter.kt
@@ -1,6 +1,5 @@
 package com.e.thepokemons.ui.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -8,11 +7,14 @@ import com.bumptech.glide.Glide
 import com.e.thepokemons.R
 import com.e.thepokemons.model.Pokemon
 
-class PokemonAdapter(var context: Context, val pokemonList: List<Pokemon>): RecyclerView.Adapter<PokemonViewModel>() {
 
+class PokemonAdapter()
+    : RecyclerView.Adapter<PokemonViewModel>() {
+
+private val pokemonList = mutableListOf<Pokemon>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewModel {
-        val view = LayoutInflater.from(context)
+        val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.pokemon_list_item, parent, false)
         return PokemonViewModel(view)
 
@@ -22,9 +24,17 @@ class PokemonAdapter(var context: Context, val pokemonList: List<Pokemon>): Recy
     }
 
     override fun onBindViewHolder(holder: PokemonViewModel, position: Int) {
-        Glide.with(context).load(pokemonList[position].image).into(holder.pokemonImage)
+        Glide.with(holder.pokemonImage.getContext())
+            .load(pokemonList[position].image)
+            .into(holder.pokemonImage)
         holder.pokemonName.text = pokemonList[position].name
 
+    }
+
+    fun setData(data: List<Pokemon>) {
+        this.pokemonList.clear()
+        this.pokemonList.addAll(data)
+        notifyDataSetChanged()
     }
 
 }
