65c46cbb1751df669835a394c4b9ff6f0976f659
Rita Casiello <rituzka@gmail.com>
2020-07-18 20:25:42 +0200

    method post register user api service

--- Week8/BlockbusterRecycler/app/src/main/java/com/e/blockbusterrecycler/networking/RemoteApi.kt
+++ Week8/BlockbusterRecycler/app/src/main/java/com/e/blockbusterrecycler/networking/RemoteApi.kt
@@ -2,8 +2,11 @@ package com.e.blockbusterrecycler.networking
 
 import com.google.gson.Gson
 
-const val BASE_URL = ""
+const val BASE_URL = "http://www.omdbapi.com/?"
 
 class RemoteApi(private val remoteApiService: RemoteApiService) {
     private val gson =  Gson()
+
+    fun registerUser()
+
 }
--- Week8/BlockbusterRecycler/app/src/main/java/com/e/blockbusterrecycler/networking/RemoteApiService.kt
+++ Week8/BlockbusterRecycler/app/src/main/java/com/e/blockbusterrecycler/networking/RemoteApiService.kt
@@ -1,7 +1,14 @@
 package com.e.blockbusterrecycler.networking
 
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface RemoteApiService {
 
-
+@POST("/api/register")
+fun registerUser(@Body request: RequestBody: Call<ResponseBody>)
 
 }
